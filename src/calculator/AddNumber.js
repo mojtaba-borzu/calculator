46cf31aea92086deaae2e7d308adfa05667fb0ad
mojtaba-borzu <mojtaba.borzu69@gmail.com>
2021-06-16 01:56:40 +0430

    change Additinal

--- src/calculator/AddNumber.js
+++ src/calculator/AddNumber.js
@@ -4,17 +4,30 @@ function AddNumber () {
   const [equal, setEqual] = useState ([]);
   const[numOne,setNumOne]=useState()
    const[numTwo,setNumTwo]=useState()
-   let numberOne =numOne
-   let numberTwo =numTwo
-  let addNum;
+   const[equalMathOne,setEqualMathOne]=useState('')
+
+   let inputNumOne =numOne
+   let inputNumTwo =numTwo
+   let numberOne 
+   let numberTwo
+   let addNum;
   let remainderNum = [];
   let addExcess = [0];
+  let equalMath 
+  let addNumTwo
+let addExcessTwo=[0]
+let endAdd=[]
+
+
 
     const AddNumberTo = () => {
-    numberOne =(numberOne.split('')).reverse()
-    numberTwo =(numberTwo.split('')).reverse()
-    console.log(numberOne)
-    console.log(numberTwo)
+      numberOne =(inputNumOne.split('')).reverse()
+      numberTwo =(inputNumTwo.split('')).reverse()
+      
+// (inputNumOne.length- inputNumTwo.length)>=0 ? `${numberTwo=inputNumTwo} ${numberOne=inputNumOne}`:0
+    
+    console.log(inputNumOne)
+    console.log(inputNumTwo)
     for (let i = 0; i < numberOne.length; i++) {
       for (let j = 0; j < numberOne.length - numberTwo.length; j++) {
         numberTwo.push ('0');
@@ -22,22 +35,50 @@ function AddNumber () {
       addNum = Number (numberOne[i]) + Number (numberTwo[i]);
       remainderNum.push (addNum % 10);
       addExcess.push (addNum >= 10 ? (addNum-(addNum%10))/10 : 0);
-      console.log((addNum-(addNum%10))/10 );
-    equal.push ((remainderNum[i] + addExcess[i] === 10)?`${0} ${(remainderNum[i] + addExcess[i]).unshift((addNum-(addNum%10))/10)}` :remainderNum[i] + addExcess[i]);
+  
+      addNumTwo=remainderNum[i] + addExcess[i]
+      addExcessTwo.push(addNumTwo>= 10 ? (addNumTwo-(addNumTwo%10))/10 : 0)
+
+      endAdd.push ((remainderNum[i] + addExcess[i] === 10)?0 :remainderNum[i] + addExcess[i]);
+      equal.push(endAdd[i]+addExcessTwo[i])
+   
          }
+     
+       
       equal.push (addExcess[addExcess.length - 1] === 1 ? 1 : equal.pop ());
-    setEqual (equal.reverse ().join (''));
+      equalMath=equal
+     
+     setEqualMathOne(equalMathOne+((equalMath.length)))
+               setEqual (equal.reverse ().join (''));
+        
+          
+             
+
     };
+    const onSubmit= ( )=>{
+      setNumOne()
+      setNumTwo()
+      setEqual([])
+      setEqualMathOne('')
+
+    }
+    
+   
   return (
     <div>
+      <form onSubmit={onSubmit} >
      <input type="text" value={numOne} onChange={(e)=>{setNumOne(e.target.value)}}/>
      <h3>{numOne}</h3>
      <input type="text" value={numTwo} onChange={(e)=>{setNumTwo(e.target.value)}}/>
     <h3>{numTwo}</h3> 
     <h1>  {`equal = ${equal}`}</h1>
+    <h1>{`equal math = ${equal} * 10 `} <sup>{equalMathOne}</sup>   </h1>
       <button type="button" onClick={AddNumberTo}>
         ADD NUMBER
       </button>
+      <button type='submit'>Clear</button>
+      </form>
+    
     </div>
   );
 }
